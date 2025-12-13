61735b4151596fbd755d7786f6f91094037ead92
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-12-13 21:35:40 +0530

    tailwind working

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,8 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  // Empty turbopack config to silence the warning when using webpack
+  turbopack: {},
 };
 
 export default nextConfig;
